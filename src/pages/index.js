cf5f662cf9bad0c47321957c343fb1ae41d6598e
Rishabh Jain <mailrishabh1403@gmail.com>
2019-06-07 22:51:25 +0530

    add stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import Layout from "../components/Layout"
-import SimleHero from "../components/SimpleHero"
+import StyledHero from "../components/StyledHero"
 import Banner from "../components/Banner"
 import About from "../components/Home/About"
 import Services from "../components/Home/Services"
-export default () => (
+
+export default ({data}) => (
   <Layout>
-    <SimleHero>
+    <StyledHero home={true} img={data.indexImage.childImageSharp.fluid}>
       <Banner
         title="continue exploring"
         info=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, officiis."
@@ -16,8 +17,20 @@ export default () => (
           explore tours
         </Link>
       </Banner>
-    </SimleHero>
+    </StyledHero>
     <About />
     <Services />
   </Layout>
 )
+
+export const query = graphql`
+query{
+  indexImage:file(relativePath:{eq:"bc.png"}){
+    childImageSharp{
+      fluid(quality: 90, maxWidth:100){
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+}
+`;
