1337233d9c499eb5623fba86bd0cad961e41dfe6
Rishabh Jain <mailrishabh1403@gmail.com>
2019-06-11 19:17:24 +0530

    cleanup

--- src/components/Home/LatestBlogs.js
+++ src/components/Home/LatestBlogs.js
@@ -1,22 +1,21 @@
 import React from 'react'
-import Tour from '../Tours/Tour'
 import { useStaticQuery, graphql } from 'gatsby'
 import Title from '../Title'
 import styles from '../../css/items.module.css'
 import AniLink from "gatsby-plugin-transition-link/AniLink";
-
+import BlogCard from '../Blog/BlogCard';
 
 const FeaturedTours = () => {
   return (
-    <section className={styles.tours}>
+    <section className={styles.blogs}>
       {/* <Title title="featured" subtitle="tours" /> */}
       <div className={styles.center}>
-        <Tour></Tour>
-        <Tour></Tour>
-        <Tour></Tour>
-        <Tour></Tour>
-        <Tour></Tour>
-        <Tour></Tour>
+        <BlogCard></BlogCard>
+        <BlogCard></BlogCard>
+        <BlogCard></BlogCard>
+        <BlogCard></BlogCard>
+        <BlogCard></BlogCard>
+        <BlogCard></BlogCard>
         
       </div>
       <AniLink to="/" fade className="btn-primary" >Button</AniLink>
