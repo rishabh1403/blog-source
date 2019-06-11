0f9e5473aa092624f94bd0616cde8fe4b03870cc
Rishabh Jain <mailrishabh1403@gmail.com>
2019-06-12 02:23:40 +0530

    did something

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
 exports.createPages = async ({ graphql, actions: { createPage } }) => {
   const { data: { allMarkdownRemark: { edges } } } = await graphql(`
   {
-    allMarkdownRemark(sort:{order:DESC,fields:frontmatter___date}){
+    allMarkdownRemark(sort:{order:ASC,fields:frontmatter___date}){
       edges{
         node{
           frontmatter{
@@ -20,13 +20,14 @@ exports.createPages = async ({ graphql, actions: { createPage } }) => {
     }
   }
   `)
-  edges.forEach(edge => {
-    console.log(edge)
+  edges.forEach((edge, index) => {
     createPage({
       path: edge.node.frontmatter.path,
       component: require.resolve("./src/templates/blog.js"),
       context: {
-        slug: edge.node.frontmatter.path
+        slug: edge.node.frontmatter.path,
+        prev: index === 0 ? null : edges[index - 1].node,
+        next: index === (edges.length - 1) ? null : edges[index + 1].node
       },
     })
   });
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 
 const blog = (props) => {
+  console.log(props.pageContext)
   console.log(props.data.markdownRemark)
   return (
     <div>
       {props.data.markdownRemark.frontmatter.title}
-      <div dangerouslySetInnerHTML={{__html:props.data.markdownRemark.html}} />
+      <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }} />
+      {props.pageContext.next &&
+        <Link to={props.pageContext.next.frontmatter.path}>
+          Next</Link>}
+      {props.pageContext.prev &&
+        <Link to={props.pageContext.prev.frontmatter.path}>
+          {props.pageContext.prev.frontmatter.title}</Link>}
     </div>
   )
 }
