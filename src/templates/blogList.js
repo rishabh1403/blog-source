09fddaba2b6c6c782c29f1d3a3aea2e49a5cf96d
Rishabh Jain <mailrishabh1403@gmail.com>
2019-06-18 09:56:18 +0530

    layout in pagination

--- src/templates/blogList.js
+++ src/templates/blogList.js
@@ -1,4 +1,6 @@
 import React from "react"
+import styles from '../css/items.module.css'
+import BlogCard from '../components/Blog/BlogCard'
 import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import StyledHero from "../components/StyledHero"
@@ -10,10 +12,20 @@ export default class BlogList extends React.Component {
       <Layout>
         <StyledHero home={true} img={this.props.data.stubImage.childImageSharp.fluid}>
         </StyledHero>
-        {posts.map(({ node }) => {
+        <section className={styles.blogs}>
+          {/* <Title title="featured" subtitle="tours" /> */}
+          <div className={styles.center}>
+            {posts.map((edge, index) => {
+              return <BlogCard edge={edge} key={index} />
+            })}
+
+          </div>
+          {/* <AniLink to="/page/2" fade className="btn-primary" >Older Posts</AniLink> */}
+        </section>
+        {/* {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
           return <div key={node.fields.slug}>{node.fields.slug}</div>
-        })}
+        })} */}
       </Layout>
     )
   }
@@ -30,9 +42,16 @@ export const blogListQuery = graphql`
         node {
           fields {
             slug
+            readingTime{
+              text
+            }
           }
-          frontmatter {
+          frontmatter{
             title
+            date(formatString: "MMMM Do, YYYY")
+            description
+            categories
+            path
           }
         }
       }
