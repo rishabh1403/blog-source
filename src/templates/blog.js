d21392931e4ca140701fe077dce78e0dcb977803
Rishabh Jain <mailrishabh1403@gmail.com>
2019-06-26 04:56:12 +0530

    more design

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -3,12 +3,20 @@ import { graphql, Link } from 'gatsby';
 import Layout from "../components/Layout"
 import Disqus from 'gatsby-plugin-disqus'
 import styles from '../css/singleBlog.module.css'
+import StyledHero from "../components/StyledHero"
+import Banner from "../components/Banner"
 
 const blog = (props) => {
   console.log(props.pageContext)
   console.log(props.data.markdownRemark)
   return (
     <Layout>
+      <StyledHero home={true} img={props.data.stubImage.childImageSharp.fluid}>
+      <Banner
+        title={props.data.markdownRemark.frontmatter.title}
+        info={props.data.markdownRemark.frontmatter.description}
+      />
+        </StyledHero>
     <div>
       {props.data.markdownRemark.frontmatter.title}
       <div className={styles.blog} dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }} />
@@ -29,8 +37,16 @@ export const query = graphql`
     markdownRemark(frontmatter:{path:{eq:$slug}}){
       frontmatter{
         title
+        description
       }
       html
+    }
+    stubImage:file(relativePath:{eq:"bc.png"}){
+      childImageSharp{
+        fluid(quality: 90, maxWidth:100){
+          ...GatsbyImageSharpFluid
+        }
+      }
     }
   }
 `
